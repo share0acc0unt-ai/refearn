14645389ce41fbeab7193988daa3f75e51163e8f
johndoe <30560623+goodebag@users.noreply.github.com>
2026-05-03 19:32:44 +0100

    chore: update test-creds

--- web/src/scripts/test-creds.js
+++ web/src/scripts/test-creds.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
 const uris = [
-  'mongodb://mydb_admin:Thisis_admin1st1st!@34.9.202.173:27017/mydb?authSource=admin',
-  'mongodb://mydb_admin:Thisis_admin1st1st!@34.9.202.173:27017/mydb?authSource=mydb',
-  'mongodb://admin:Thisis_admin1st1st!@34.9.202.173:27017/mydb?authSource=admin',
-  'mongodb://mydb_admin:Thisis_admin1st1st%21@34.9.202.173:27017/mydb?authSource=admin',
-  'mongodb://mydb_admin:Thisis_admin1st1st%21@34.9.202.173:27017/mydb?authSource=mydb',
+  'mongodb://[IP_ADDRESS]/mydb_admin:password123!@34.9.202.173:27017/mydb?authSource=admin',
+  'mongodb://[IP_ADDRESS]/mydb_admin:password123!@34.9.202.173:27017/mydb?authSource=admin',
 ];
 
 async function run() {
