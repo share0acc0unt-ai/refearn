b97c83cb8c701e3abc1beeec60bbb8e02a7513d5
johndoe <30560623+goodebag@users.noreply.github.com>
2026-05-06 08:55:05 +0100

    feat: link start advertising button to advertise page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
                       <h1 className="text-white text-4xl font-black leading-tight tracking-[-0.033em] @[480px]:text-5xl @[480px]:font-black @[480px]:leading-tight @[480px]:tracking-[-0.033em]">Advertise on real people's screen</h1>
                       <h2 className="text-white/80 text-sm font-normal leading-normal @[480px]:text-base @[480px]:font-normal @[480px]:leading-normal">Facts: People only buy from people they know, like or trust. Reach millions of potential customers, Join Paypulse and start advertising today.</h2>
                     </div>
-                    <Link href="/signup">
+                    <Link href="/advertise">
                       <button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-10 px-4 @[480px]:h-12 @[480px]:px-5 bg-primary text-[#112215] text-sm font-bold leading-normal tracking-[0.015em] @[480px]:text-base @[480px]:font-bold @[480px]:leading-normal @[480px]:tracking-[0.015em]">
                         <span className="truncate">Start Advertising</span>
                         <ArrowRightIcon className="ml-2 h-4 w-4" />
