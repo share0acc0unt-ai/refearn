49e8855908d88b42a6d46fab925c095df7badedf
johndoe <30560623+goodebag@users.noreply.github.com>
2026-05-09 09:13:11 +0100

    fix: ensure full coverage of forced dark theme on payment pages

--- web/src/app/advertise/payment/page.tsx
+++ web/src/app/advertise/payment/page.tsx
@@ -98,7 +98,7 @@ export default function PaymentPage() {
     }
 
     return (
-        <div className="relative flex h-auto min-h-screen w-full flex-col bg-background-light dark:bg-background-dark group/design-root overflow-x-hidden font-display">
+        <div className="relative flex h-auto min-h-screen w-full flex-col bg-background group/design-root overflow-x-hidden font-display">
             <div className="layout-container flex h-full grow flex-col">
                 <div className="px-4 md:px-10 lg:px-20 xl:px-40 flex flex-1 justify-center py-5">
                     <div className="layout-content-container flex flex-col max-w-[960px] flex-1">
